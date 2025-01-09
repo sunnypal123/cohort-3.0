360bf670a61ff1402830186c7c786f37b11ed691
sunny pal <sunnyrajupal@gmail.com>
2025-01-09 22:59:37 +0530

    main

---
+++ index.js
@@ -0,0 +1,9 @@
+let userinput = prompt("enter a number")
+let number = number(userinput);
+
+if(number %5===0){
+    console.log("the number is a multiple of 5")
+}
+    else{
+        console.log("the number is not multiple of 5");
+    }
