213944bcdc68b710c0e58501906ffdae8cc1321c
sunny pal <sunnyrajupal@gmail.com>
2025-01-13 12:29:58 +0530

    pratice-project

---
+++ practice-set/project-2/script.js
@@ -0,0 +1,21 @@
+let con = document.querySelector("#container")
+let love = document.querySelector("i")
+
+
+
+con.addEventListener("dblclick",function(){
+    love.style.transform = 'translate(-50%, -50%) scale(1)';
+    love.style.opacity = 0.8
+
+})
+setTimeout(function(){
+    love.style.opacity = 0;
+
+
+},1000);
+
+setTimeout(function(){
+    love.style.transform = 'translate(-50%, -50%) scale(1)'
+
+
+},2000)
