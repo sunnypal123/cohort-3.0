33249bbb924275ff177c60c8c186dd3fcc5c0873
sunny pal <sunnyrajupal@gmail.com>
2025-01-19 23:45:41 +0530

    task

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let userinput = prompt("enter a number")
-let number = number(userinput);
-
-if(number %5===0){
-    console.log("the number is a multiple of 5")
-}
-    else{
-        console.log("the number is not multiple of 5");
-    }
+let x = 'Tic';
+x = x + 'Tac';
+x = x + x;
